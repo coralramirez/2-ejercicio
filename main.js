ed3b718134303be18003d3080e17c228d967428d
PatriciaMendozas <patriciamendozasoncco@gmail.com>
2016-12-28 09:39:04 -0500

    subo archivo

---
+++ main.js
@@ -0,0 +1,37 @@
+
+function calcular()
+{
+	var vacation = document.getElementById("vacaciones").value;
+	var weekday = parseInt(document.getElementById("dias").value);
+
+	var isVacation = true;
+	var isWeekday = false;
+
+	var salida = document.getElementById("salida");
+
+	if(vacation == "2")
+	{
+		isVacation = false;
+	}
+
+	if(weekday < 6)
+	{
+		isWeekday = true;
+	}
+
+      if(isVacation)
+	  {
+		 salida.innerHTML = "1";
+	  }
+       else
+      {
+		 if(isWeekday)
+         {
+			salida.innerHTML = "0";
+         }
+         else
+         {
+			salida.innerHTML = "1";
+		 }
+	  }
+}
